59666e184ba2e3f820093d4c1e2bdfb521e0a6cc
Matthew Nekirov <matthew.nekirov@gmail.com>
2023-11-15 18:12:07 +0300

    WIP

--- benchmark/bench_dataset_traverse.cpp
+++ benchmark/bench_dataset_traverse.cpp
@@ -1,7 +1,8 @@
-#include <dataloader.hpp>
+#include <cstring>
 #include <iostream>
-#include <benchmark/benchmark.h>
 #include <immintrin.h>
+#include <dataloader.hpp>
+#include <benchmark/benchmark.h>
 
 static std::vector<Vec2> dataset() {
   CSVLoader loader("../dataset.csv");
@@ -19,13 +20,26 @@ static std::pair<vofd, vofd> dataset_2() {
   return {x, y};
 }
 
-using vofv = std::vector<__m256>;
+using vofv = std::vector<__m256d>;
 static std::pair<vofv, vofv> dataset_3() {
   auto v = dataset();
-  vofv x(v.size() / 4), y(v.size() / 4);
-  for (size_t i = 0; i < x.size(); ++i) {
+  const size_t nvec = (v.size() + 3) & ~0b11;
+
+  vofv x(nvec / 4), y(nvec / 4);
+  for (size_t i = 0; i < v.size() / 4; ++i) {
     x[i] = _mm256_set_pd(v[4*i].x1(), v[4*i+1].x1(), v[4*i+2].x1(), v[4*i+3].x1());
-    x[i] = _mm256_set_pd(v[4*i].x2(), v[4*i+1].x2(), v[4*i+2].x2(), v[4*i+3].x2());
+    y[i] = _mm256_set_pd(v[4*i].x2(), v[4*i+1].x2(), v[4*i+2].x2(), v[4*i+3].x2());
+  }
+
+  // process tail
+  if (nvec != v.size()) {
+    alignas(32) double xs[4] = {0}, ys[4] = {0};
+    for (size_t i = nvec - 4; i < v.size(); ++i) {
+      const size_t j = i + 4 - nvec;
+      xs[j] = v[i].x1(), ys[j] = v[i].x2();
+    }
+    x[nvec/4-1] = _mm256_load_pd(xs);
+    y[nvec/4-1] = _mm256_load_pd(ys);
   }
   return {x, y};
 }
@@ -46,7 +60,6 @@ static void linear_traverse(benchmark::State& state) {
     benchmark::DoNotOptimize(xy);
   }
 }
-BENCHMARK(linear_traverse);
 
 static void double_linear_traverse(benchmark::State& state) {
   auto [ax, ay] = dataset_2();
@@ -64,74 +77,125 @@ static void double_linear_traverse(benchmark::State& state) {
     benchmark::DoNotOptimize(xy);
   }
 }
-BENCHMARK(double_linear_traverse);
 
 static void avx_traverse(benchmark::State& state) {
   auto&&[ax, ay] = dataset_2();
   for (auto _ : state) {
-    __m256  x = _mm256_set1_pd(0),
+    __m256d  x = _mm256_set1_pd(0),
             y = _mm256_set1_pd(0),
            xy = _mm256_set1_pd(0),
            xS = _mm256_set1_pd(0);
-    for (size_t i = 0; i < ax.size(); i += 4) {
-      __m256 xi = _mm256_loadu_pd(&ax[i]);
+    size_t i;
+    for (i = 0; i < ax.size(); i += 4) {
+      const __m256d xi = _mm256_loadu_pd(&ax[i]);
       x = _mm256_add_pd(x, xi);
 
-      __m256 yi = _mm256_loadu_pd(&ay[i]);
+      const __m256d yi = _mm256_loadu_pd(&ay[i]);
       y = _mm256_add_pd(y, yi);
 
-      yi = _mm256_mul_pd(yi, xi);
-      xy = _mm256_add_pd(yi, xy);
-
-      xi = _mm256_mul_pd(xi, xi);
-      xS = _mm256_add_pd(xS, xi);
+      xy = _mm256_fmadd_pd(yi, xi, xy);
+      xS = _mm256_fmadd_pd(xi, xi, xS);
     }
-    alignas(32) double bx[4], by[4], bxS[4], bxy[4];
-    _mm256_store_pd(bx, x);
-    _mm256_store_pd(by, y);
-    _mm256_store_pd(bxS, xS);
-    _mm256_store_pd(bxy, xy);
+    alignas(32) double r[4];
+    __m256d sum = _mm256_hadd_pd(x, y);
+    __m128d sum_high = _mm256_extractf128_pd(sum, 1);
+    __m128d res = _mm_add_pd(sum_high, _mm256_castpd256_pd128(sum));
+    _mm_store_pd(r, res);
 
-    benchmark::DoNotOptimize(bx);
-    benchmark::DoNotOptimize(by);
-    benchmark::DoNotOptimize(bxS);
-    benchmark::DoNotOptimize(bxy);
+    sum = _mm256_hadd_pd(xS, xy);
+    sum_high = _mm256_extractf128_pd(sum, 1);
+    res = _mm_add_pd(sum_high, _mm256_castpd256_pd128(sum));
+    _mm_store_pd(r + 2, res);
+
+    for (; i < ax.size(); ++i) {
+      r[0] += ax[i];
+      r[1] += ay[i];
+      r[2] += ax[i] * ax[i];
+      r[3] += ay[i] * ax[i];
+    }
+
+    benchmark::DoNotOptimize(r);
   }
 }
-BENCHMARK(avx_traverse);
 
 static void aligned_avx_traverse(benchmark::State& state) {
-  auto [ax, ay] = dataset_3();
+  auto&&[ax, ay] = dataset_3();
   for (auto _ : state) {
-    __m256  x = _mm256_set1_pd(0),
+    __m256d x = _mm256_set1_pd(0),
            y = _mm256_set1_pd(0),
            xy = _mm256_set1_pd(0),
            xS = _mm256_set1_pd(0);
-    for (size_t i = 0; i < ax.size(); i += 4) {
-      __m256 xi = ax[i];
+
+    for (size_t i = 0; i < ax.size(); ++i) {
+      const __m256d xi = ax[i];
       x = _mm256_add_pd(x, xi);
 
-      __m256 yi = ay[i];
+      const __m256d yi = ay[i];
       y = _mm256_add_pd(y, yi);
 
-      yi = _mm256_mul_pd(yi, xi);
-      xy = _mm256_add_pd(yi, xy);
-
-      x = _mm256_mul_pd(x, x);
-      xS = _mm256_add_pd(xS, x);
+      xy = _mm256_fmadd_pd(yi, xi, xy);
+      xS = _mm256_fmadd_pd(xi, xi, xS);
     }
-    alignas(32) double bx[4], by[4], bxS[4], bxy[4];
-    _mm256_store_pd(bx, x);
-    _mm256_store_pd(by, y);
-    _mm256_store_pd(bxS, xS);
-    _mm256_store_pd(bxy, xy);
 
-    benchmark::DoNotOptimize(bx);
-    benchmark::DoNotOptimize(by);
-    benchmark::DoNotOptimize(bxS);
-    benchmark::DoNotOptimize(bxy);
+    alignas(32) double r[4];
+    __m256d sum = _mm256_hadd_pd(x, y);
+    __m128d sum_high = _mm256_extractf128_pd(sum, 1);
+    __m128d res = _mm_add_pd(sum_high, _mm256_castpd256_pd128(sum));
+    _mm_store_pd(r, res);
+
+    sum = _mm256_hadd_pd(xS, xy);
+    sum_high = _mm256_extractf128_pd(sum, 1);
+    res = _mm_add_pd(sum_high, _mm256_castpd256_pd128(sum));
+    _mm_store_pd(r + 2, res);
+
+    benchmark::DoNotOptimize(r);
   }
 }
+
+
+#define BENCH(func)                                             \
+  BENCHMARK_PRIVATE_DECLARE(func) =                             \
+      (::benchmark::internal::RegisterBenchmarkInternal(        \
+          new ::benchmark::internal::FunctionBenchmark(         \
+              #func,                                            \
+              [](::benchmark::State& st) {                      \
+                  FlushCacheNonpausing();                       \
+                  func(st);                                     \
+              }                                                 \
+          )                                                     \
+       ))                                                       \
+    ->Iterations(10)
+
+inline void FlushCacheNonpausing(int level = 3) {
+  const auto& cpu = benchmark::CPUInfo::Get();
+
+  std::vector<unsigned> resetter;
+  for (const auto& cache : cpu.caches) {
+    if (cache.level != level) {
+      continue;
+    }
+
+    resetter.resize(cache.size / sizeof(unsigned),
+                    static_cast<unsigned>(level));
+  }
+
+  if (resetter.empty()) {
+    throw std::logic_error("Failed to reset cache");
+  }
+
+  unsigned trash = static_cast<unsigned>(level * level);
+  for (unsigned& v : resetter) {
+    trash += v;
+    v += trash;
+  }
+
+  benchmark::DoNotOptimize(trash);
+  benchmark::DoNotOptimize(resetter);
+}
+
+BENCHMARK(linear_traverse);
+BENCHMARK(double_linear_traverse);
+BENCHMARK(avx_traverse);
 BENCHMARK(aligned_avx_traverse);
 
 BENCHMARK_MAIN();
--- cmake/CompilerOptions.cmake
+++ cmake/CompilerOptions.cmake
@@ -5,7 +5,7 @@ set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
 add_compile_options("-pipe" "-g" "-Wall" "-Wextra" "-Wpedantic")
-add_compile_options("-mavx")
+add_compile_options("-mavx2" "-mfma")
 
 set(CMAKE_CXX_FLAGS_RELEASE "-O2")
 if (CMAKE_BUILD_TYPE MATCHES "Release")
